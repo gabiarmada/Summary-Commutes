e7429a56e547b55abd7b5701f45503e14034687c
Gabriella Armada <garmada@masonlive.gmu.edu>
2021-05-15 21:59:28 -0400

    integrate GMU_commute and GMU_commute-jrk

--- GMU_commute.R
+++ GMU_commute.R
@@ -1,33 +1,50 @@
 #' \code{GMU_commute} Function to read the GMU commute data 
 #' 
 #' @title GMU_commute
-#' @param csvfile .csv filename containing the GMU commute data
-#' @author Gabi Armada 
+#' @param file .csv filename containing the GMU commute data
+#' @param output string of the desired function output; accepted values are: "trip summary", "overall summary", or "df"
+#' @author Gabi Armada and Jenna Krall 
 #' @export
 
 library(tidyverse)
 library(lubridate)
-
-GMU_commute <- function(file){
+library(data.table)
+GMU_commute <- function(file, output){
   headers <- read.csv(file, header = F, nrows = 1, as.is = T)
-  df1 <- read.csv(file, skip = 3, header = F)
-  colnames(df1) = headers 
+  headers <- as.character(headers[, c(2:18)])
+  df1 <- fread(file, skip = 3, select = c(2 : 18))
+  colnames(df1) = headers
   
   total_trips <- n_distinct(df1$Trip)
   
   df2 <- df1 %>% 
           select("Date & Time", "Latitude", "Longitude", "Trip", "Trip duration", "Trip distance")
   
-  df2 <- df2 %>% group_by(Trip) %>% 
-        mutate(trip_total_time = max(`Trip duration`))
+  df2 <- df2 %>% 
+          group_by(Trip) %>% 
+          mutate(trip_total_time = max(`Trip duration`))
   df2 <- df2[!(df2$trip_total_time < "00:05:00"),] 
-  df2 <- df2[df2$Latitude != 0, ] 
-  df2 <- df2[df2$Longitude !=0, ]
   
-  actual_trips <- n_distinct(df2$Trip) 
-  percent_usable_trips <- (actual_trips/total_trips)*100
-  return (c(total_trips, actual_trips, percent_usable_trips))
+  # create variable for missingness (for %)
+  df2 <- mutate(df2, missing = ifelse(Latitude == 0 | Longitude == 0, 0, 1))
+  actual_trips <- filter(df2, missing == 1) %>% 
+                  select(Trip) %>% 
+                  n_distinct() 
+  
+  # trip summary (% missing)
+  trip_summaries <- summarize(df2, nrows = n(), sum = sum(missing), mean = mean(missing))
+  
+  # overall summary
+  overall_summary<- ungroup(df2) %>% 
+                    summarize(nrows = n(), 
+                              sum = sum(missing),
+                              mean = round(mean(missing), digits = 3)) %>%
+                    mutate(total_trips = total_trips, actual_trips = actual_trips)
+  
+  if (output == "trip summary"){
+    return (trip_summaries)}
+    else if (output == "overall summary"){
+      return(overall_summary)} 
+    else if(output == "df"){
+      return (df2)}
 }
-
-
-
